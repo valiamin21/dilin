bf46ceb91c755d9f88caae552bc02f6d1d2ebdc4
Amin <valiamin21@gmail.com>
2019-11-11 09:45:42 +0330

    adding adapter for recyclerView in BookmarkedWordsFragment but adapter events are not implemented yet

--- app/src/main/java/ir/proglovving/dilin/views/fragment/BookmarkedWordsFragment.java
+++ app/src/main/java/ir/proglovving/dilin/views/fragment/BookmarkedWordsFragment.java
@@ -2,13 +2,21 @@ package ir.proglovving.dilin.views.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import ir.proglovving.dilin.R;
+import ir.proglovving.dilin.adapters.WordsRecyclerViewAdapter;
+import ir.proglovving.dilin.data_model.Word;
+import ir.proglovving.dilin.database_open_helpers.WordsOpenHelper;
 
-public class BookmarkedWordsFragment extends Fragment {
+public class BookmarkedWordsFragment extends Fragment implements WordsRecyclerViewAdapter.EventOfWordMeaningRecyclerView {
+
+    private RecyclerView recyclerView;
 
     public BookmarkedWordsFragment() {
         // Required empty public constructor
@@ -23,9 +31,28 @@ public class BookmarkedWordsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bookmarked_words, container, false);
+        View view = inflater.inflate(R.layout.fragment_bookmarked_words,container,false);
+        recyclerView = view.findViewById(R.id.recycler_view_bookmarked_fragment);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false));
+
+        WordsRecyclerViewAdapter adapter = new WordsRecyclerViewAdapter(getContext(),WordsOpenHelper.getDetailedWordList(getContext(),true),this);
+        recyclerView.setAdapter(adapter);
+        return view;
     }
 
 
+    @Override
+    public void onDeleteClick(Word word, int position) {
+        Toast.makeText(getContext(), "deleted " + word.getWord(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onBookmarkClick(Word word) {
+        Toast.makeText(getContext(), "bookmarked " + word.getWord(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onEditClick(Word word, int position) {
+        Toast.makeText(getContext(), "edited " + word.getWord(), Toast.LENGTH_SHORT).show();
+    }
 }
