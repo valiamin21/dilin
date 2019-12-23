d46789decd294076ec7f6ea83ae49f2461f49fb8
Amin <valiamin21@gmail.com>
2019-12-23 23:06:46 +0330

    removing some addition comments in AppIntroActivity

--- app/src/main/java/ir/proglovving/dilin/views/activity/AppIntroActivity.java
+++ app/src/main/java/ir/proglovving/dilin/views/activity/AppIntroActivity.java
@@ -21,23 +21,7 @@ public class AppIntroActivity extends AppIntro {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_app_intro);
-
         setDoneText("فهمیدم");
-
-//        SliderPage sliderPage = new SliderPage();
-//        sliderPage.setTitle("my slider");
-//        sliderPage.setDescription("This is dilin a wonderful app");
-//        sliderPage.setImageDrawable(R.drawable.programmer_profile_picture);
-//        sliderPage.setBgColor(ContextCompat.getColor(this,R.color.primary_light));
-//        addSlide(IntroFragment.newInstance(sliderPage));
-//
-//        sliderPage = new SliderPage();
-//        sliderPage.setTitle("second title");
-//        sliderPage.setDescription("it is second descriptions");
-//        sliderPage.setImageDrawable(R.drawable.programmer_profile_picture1);
-//        sliderPage.setBgColor(ContextCompat.getColor(this,R.color.colorAccent));
-//        addSlide(IntroFragment.newInstance(sliderPage));
 
         IntroFragment introFragment = IntroFragment.newInstance("this my fucking title","this is my fucking description",R.drawable.programmer_profile_picture1);
         addSlide(introFragment);
