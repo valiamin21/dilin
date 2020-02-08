b7d1443d8c8e43f16b304b5742548c8990351a94
Amin <valiamin21@gmail.com>
2020-02-08 21:47:42 +0330

    modifying todo list

--- app/src/main/java/ir/proglovving/dilin/MyApplication.java
+++ app/src/main/java/ir/proglovving/dilin/MyApplication.java
@@ -18,7 +18,6 @@ public class MyApplication extends Application {
      */
 
     // releasing process
-    // TODO: 2/6/20 implementing rating and other apps items in navigationView
     // TODO: 2/6/20 modifying search button in app
     // TODO: 9/7/19 releasing app in markets
 
