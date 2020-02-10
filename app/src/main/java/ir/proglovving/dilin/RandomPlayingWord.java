2ef738cd66dbc78b24efaa1a8a9ea7d72cc2a432
Amin <valiamin21@gmail.com>
2020-02-10 16:23:27 +0330

    widget playing meghod was changed (all words from all notebooks will be played)

--- app/src/main/java/ir/proglovving/dilin/RandomPlayingWord.java
+++ app/src/main/java/ir/proglovving/dilin/RandomPlayingWord.java
@@ -12,82 +12,37 @@ import ir.proglovving.dilin.database_open_helpers.NotebookOpenHelper;
 import ir.proglovving.dilin.database_open_helpers.WordsOpenHelper;
 
 public class RandomPlayingWord {
-//    private static int loopTimes = 0; // تعداد دفعات تکرار تابع getWord در صورت خالی بودن دفتر یا کلمه
-
-//    public static Word getWord(Context context){
-//
-//        Word voc;
-//
-//        Notebook notebook = new NotebookOpenHelper(context).getRandomIsPlayingNotebook();
-//        if(notebook == null){
-//            voc = new Word();
-//            voc.setWord(context.getString(R.string.there_is_nothing_to_play));
-//            voc.setMeaning(context.getString(R.string.play_list_is_empty));
-//            return voc;
-//        }
-//
-//        voc = new WordsOpenHelper(context,notebook.getNoteBookName()).getRandomWord();
-//
-//        return voc;
-//    }
-
-//    public static int randInt(int min, int max) {
-//        Random rand = new Random();
-//        return rand.nextInt((max - min) + 1) + min;
-//    }
 
     public static int randInt(int min, int max) { // خود مین و ماکس هم جزو خروجی باشه
         Random random = new Random();
         return random.nextInt(max-min+1) + min;
     }
 
-//    private int randInt(int min, int max){
-//        Random random = new Random();
-//        return random.nextInt(max - min -1)+min +1;
-//    }
-
-//    private static int chancePointer;
-
-//    public static Word getWord(Context context){
-//        Word voc;
-//
-//        Notebook notebook = new NotebookOpenHelper(context).getRandomIsPlayingNotebook();
-//        if(notebook == null){
-//            voc = new Word();
-//            voc.setWord(context.getString(R.string.there_is_nothing_to_play));
-//            voc.setMeaning(context.getString(R.string.play_list_is_empty));
-//            return voc;
-//        }
-//
-//
-//        chancePointer = 0;
-//
-//        //  در خط زیر اوّلی عدد ورودی و دوّمی نام دفتر است.
-//        HashMap<Integer,String> hashMap = new HashMap<>();
-//
-//        List<String> notebooksNames = new NotebookOpenHelper(context).getNotebooksNames();
-//        for (int i = 0; i < notebooksNames.size(); i++) {
-//            int vocsCount = new WordsOpenHelper(context, notebooksNames.get(i)).getRawsCount();
-//
-//            for (int j = 0; j < vocsCount; j++) {
-//                chancePointer++;
-//                hashMap.put(chancePointer,notebooksNames.get(i));
-//            }
-//
-//        }
-//
-//        String selectedNotebookName = hashMap.get(randInt(1,chancePointer));
-//
-//
-//        voc = new WordsOpenHelper(context,selectedNotebookName).getRandomWord();
-//
-//        return voc;
-//    }
-
-    public static Word getWordUsingOtherMethod(Context context){
+    public static Word getRandomWordFromPlayingNotebook(Context context){
         List<Word> wordList = new ArrayList<>();
 
         List<Notebook> notebooks = new NotebookOpenHelper(context).getIsPlayingNotebooks();
+        for (int i = 0; i < notebooks.size(); i++) {
+            List<Word> subWordList = new WordsOpenHelper(context,notebooks.get(i).getId()).getWordList(false);
+            for (int j = 0; j < subWordList.size(); j++) {
+                wordList.add(subWordList.get(j));
+            }
+        }
+
+        if(wordList.size() == 0){ // اگر چیزی برای پخش موجود نبود
+            Word word = new Word();
+            word.setWord(context.getString(R.string.there_is_nothing_to_play));
+            word.setMeaning(context.getString(R.string.play_list_is_empty));
+            return word;
+        }else{
+            return wordList.get(randInt(0, wordList.size()-1));
+        }
+    }
+
+    public static Word getARandomWord(Context context){
+        List<Word> wordList = new ArrayList<>();
+
+        List<Notebook> notebooks = new NotebookOpenHelper(context).getNotebookList();
         for (int i = 0; i < notebooks.size(); i++) {
             List<Word> subWordList = new WordsOpenHelper(context,notebooks.get(i).getId()).getWordList(false);
             for (int j = 0; j < subWordList.size(); j++) {
--- app/src/main/java/ir/proglovving/dilin/widgets/ShowWordsWidget.java
+++ app/src/main/java/ir/proglovving/dilin/widgets/ShowWordsWidget.java
@@ -36,13 +36,7 @@ public class ShowWordsWidget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-
-//            ShowWordsWidgetOpenHelper openHelper = new ShowWordsWidgetOpenHelper(context);
-//            Word word = openHelper.getPlayWord();
-
-//            updateAppWidget(context, appWidgetManager, appWidgetId, word);
-
-            Word word = RandomPlayingWord.getWordUsingOtherMethod(context);
+            Word word = RandomPlayingWord.getARandomWord(context);
             if (word != null) {
                 updateAppWidget(context, appWidgetManager, appWidgetId, word);
             }
