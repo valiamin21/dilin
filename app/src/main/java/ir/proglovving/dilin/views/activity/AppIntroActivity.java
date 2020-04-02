c0a5e3cba972cf23c1107ba87d793350dc0efc16
Amin <valiamin21@gmail.com>
2020-04-02 11:25:51 +0430

    making AppIntroActivity full screen

--- app/src/main/java/ir/proglovving/dilin/views/activity/AppIntroActivity.java
+++ app/src/main/java/ir/proglovving/dilin/views/activity/AppIntroActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.WindowManager;
 
 import com.github.paolorotolo.appintro.AppIntro;
 
@@ -24,6 +25,10 @@ public class AppIntroActivity extends AppIntro {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // making full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setDoneText("فهمیدم");
 
         // hiding skipButton
